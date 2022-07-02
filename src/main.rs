95ee499fb9556e6e8dc71dabfd0d221b1803f915
Andreas Mårtensson <andreas@addem.se>
2022-07-03 01:57:40 +0200

    first parse

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ use pest::Parser;
 pub struct CUEParser;
 
 fn main() {
-    // let successful_parse = CUEParser::parse(Rule::BasicLit, "-273.15");
-    // println!("{:?}", successful_parse);
+    let successful_parse = CUEParser::parse(Rule::Expression, "-273.15");
+    println!("{:#?}", successful_parse);
 
-    // let unsuccessful_parse = CUEParser::parse(Rule::BasicLit, "this is not a number");
-    // println!("{:?}", unsuccessful_parse);
+    let unsuccessful_parse = CUEParser::parse(Rule::Expression, "this is not a number");
+    println!("{:?}", unsuccessful_parse);
 }
