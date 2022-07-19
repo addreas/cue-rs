d840bf014769e33e6744a8a5e6b02d11dd75c5c2
Andreas Mårtensson <andreas@addem.se>
2022-07-19 21:06:34 +0200

    irrelevant

--- src/txtar.rs
+++ src/txtar.rs
@@ -6,43 +6,49 @@ use std::io::prelude::*;
 use std::path::Path;
 
 #[derive(Debug)]
-pub struct TxTar {
-    header: String,
-    sections: HashMap<String, String>,
+pub struct TxTar<'a> {
+    header: &'a str,
+    sections: HashMap<&'a str, &'a str>,
+}
+
+impl<'a> TxTar<'a> {
+    pub fn header(&self) -> &str {
+        &self.header
+    }
+    pub fn get_section(&self, key: &'a str) -> Option<&'a str> {
+        self.sections.get(key).map(|s| *s)
+    }
 }
 
 const BEGIN_MARKER: &str = "-- ";
 const END_MARKER: &str = " --";
 
-pub fn parse(f: &mut dyn Read) -> Result<TxTar, std::io::Error> {
-    let mut buf = String::new();
-    f.read_to_string(&mut buf)?;
+pub fn parse<'a>(buf: &'a str) -> TxTar<'a> {
     let pat = Regex::new("-- .* --").expect("infalible");
     let parts: Vec<_> = buf.split_inclusive(&pat).collect();
 
     let (header, mut current_name) = match parts[0].rsplit_once("\n") {
-        Some((h, f)) => (h.to_string(), f),
-        None => ("".to_string(), parts[0]),
+        Some((h, f)) => (h, f),
+        None => ("", parts[0]),
     };
 
     let mut sections = HashMap::new();
     for part in parts[1..].iter() {
         let (content, next_name) = match part.rsplit_once("\n") {
-            Some((h, f)) => (h.to_string(), f),
-            None => ("".to_string(), *part),
+            Some((h, f)) => (h, f),
+            None => ("", *part),
         };
         sections.insert(
             current_name
                 .trim_start_matches(BEGIN_MARKER)
                 .trim_end_matches(END_MARKER)
-                .trim()
-                .to_string(),
-            content.trim_start().to_string(),
+                .trim(),
+            content.trim_start(),
         );
         current_name = next_name;
     }
 
-    return Ok(TxTar { header, sections });
+    return TxTar { header, sections };
 }
 
 #[test]
@@ -52,6 +58,8 @@ fn test_export_007() {
         // "../../cue-lang/cue/cue/testdata/export/008.txtar",
     ))
     .unwrap();
-    let tar = parse(&mut f);
+    let mut buf = String::new();
+    f.read_to_string(&mut buf).expect("file exists");
+    let tar = parse(buf.as_str());
     println!("tar: {:#?}", tar);
 }
